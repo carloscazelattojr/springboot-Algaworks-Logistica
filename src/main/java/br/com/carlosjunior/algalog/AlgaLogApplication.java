7b8575432adc2be3bc2d890a1bb8387ec3c96c07
Carlos Cazelatto Junior <carlosjunior1983@gmail.com>
2021-08-15 14:32:45 -0300

    Projeto Alga-Log criado

---
+++ src/main/java/br/com/carlosjunior/algalog/AlgaLogApplication.java
@@ -0,0 +1,13 @@
+package br.com.carlosjunior.algalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgaLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/carlosjunior/algalog/AlgaLogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carlosjunior.algalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgaLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
